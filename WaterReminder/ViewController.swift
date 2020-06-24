35fdf545a721d649bef399ecd0ad32e243037e70
Vladislav Likhachev <mymindissharp@gmail.com>
2020-06-24 17:15:49 +0300

    Initial Commit

---
+++ WaterReminder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WaterReminder
+//
+//  Created by Владислав Лихачев on 24.06.2020.
+//  Copyright © 2020 Vladislav Likhachev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
